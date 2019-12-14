d12089efc9b2a2df4a3db014d240bde2cc0dd45f
Dan Xu <danxu@Dans-MacBook-Pro.local>
2019-12-13 20:02:26 -0800

    update the instances DNS

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ let app = express();
 app.use(express.static('public'));
 app.use(cors());
 
-app.use(proxy('/api/restaurant', {target: 'http://ec2-52-53-160-167.us-west-1.compute.amazonaws.com:3001/'}));
+app.use(proxy('/api/restaurant', {target: 'http://ec2-18-144-27-107.us-west-1.compute.amazonaws.com:3001/'}));
 app.use(proxy('/restaurantid', {target: 'http://ec2-54-193-61-50.us-west-1.compute.amazonaws.com:3004/'}));
 app.use(proxy('/restaurant', {target: 'http://localhost:3002'}));
-app.use(proxy('/api/dinner', {target: 'http://ec2-54-193-61-50.us-west-1.compute.amazonaws.com:3003/'}));
-app.use(proxy('/api/wine', {target: 'http://ec2-54-193-61-50.us-west-1.compute.amazonaws.com:3003/'}));
+app.use(proxy('/api/dinner', {target: 'http://ec2-18-144-27-107.us-west-1.compute.amazonaws.com:3003/'}));
+app.use(proxy('/api/wine', {target: 'http://ec2-18-144-27-107.us-west-1.compute.amazonaws.com:3003/'}));
 
 
 app.listen(port, () => {
