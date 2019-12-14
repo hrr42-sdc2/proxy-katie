451bcc8d78550d66b5dd7be5f2e1fffab2279fd6
Dan Xu <danxu@dans-mbp.lan>
2019-12-13 16:18:24 -0800

    connect overview and menu to proxy

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ let app = express();
 app.use(express.static('public'));
 app.use(cors());
 
-app.use(proxy('/api/restaurant', {target: 'http://localhost:3001/'}));
+app.use(proxy('/api/restaurant', {target: 'http://ec2-52-53-160-167.us-west-1.compute.amazonaws.com:3001/'}));
 app.use(proxy('/restaurantid', {target: 'http://ec2-52-53-155-238.us-west-1.compute.amazonaws.com:3004/'}));
 app.use(proxy('/restaurant', {target: 'http://localhost:3002'}));
+app.use(proxy('/api/dinner', {target: 'http://ec2-52-53-160-167.us-west-1.compute.amazonaws.com:3003/'}));
+app.use(proxy('/api/wine', {target: 'http://ec2-52-53-160-167.us-west-1.compute.amazonaws.com:3003/'}));
+
 
 app.listen(port, () => {
   console.log('Port 3000 is listening');
